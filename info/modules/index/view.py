8b3d5e19161e7b572689cea4cde8123777274cb7
夜雨生虹 <985609191@qq.com>
2020-08-20 15:50:22 +0800

    图片验证码集成

--- info/modules/index/view.py
+++ info/modules/index/view.py
@@ -1,8 +1,13 @@
 from . import index_blue
 from info import redis_store
-from flask import render_template
+from flask import render_template,current_app
 
 @index_blue.route('/',methods=['GET','POST'])
 def index():
 
-    return render_template()
+    return render_template('news/index.html')
+
+#处理网站logo
+@index_blue.route('/favicon.ico')
+def get_web_logo():
+    return current_app.send_static_file('news/favicon.ico')
